6383bc38b534da524cc6163ab8ba70c093d834d2
Spencer <scarvill91@gmail.com>
2015-11-19 11:45:55 -0600

    Revamp ServerRunner / cobspec router to use new handlers

--- src/main/java/scarvill/httpserver/cobspec/Cobspec.java
+++ src/main/java/scarvill/httpserver/cobspec/Cobspec.java
@@ -1,10 +1,13 @@
 package scarvill.httpserver.cobspec;
 
 import scarvill.httpserver.Request;
+import scarvill.httpserver.Resource;
 import scarvill.httpserver.Response;
 import scarvill.httpserver.Router;
 import scarvill.httpserver.constants.Method;
 import scarvill.httpserver.constants.Status;
+import scarvill.httpserver.handlers.ChangeResourceHandler;
+import scarvill.httpserver.handlers.GetResourceHandler;
 import scarvill.httpserver.handlers.IndifferentHandler;
 import scarvill.httpserver.handlers.RouteHandler;
 
@@ -12,28 +15,38 @@ import java.util.HashMap;
 import java.util.function.Function;
 
 public class Cobspec {
-    private static final Function<Request, Response> STATUS_OK_HANDLER =
-        new IndifferentHandler(new Response(Status.OK));
     private static final Function<Request, Response> REDIRECT_HANDLER =
         new IndifferentHandler(
-            new Response(Status.FOUND, new String[]{"Location: http://localhost:5000\r\n"}));
+            new Response(Status.FOUND, new String[]{"Location: http://localhost:5000/\r\n"}));
 
     public static Router configuredRouter() {
         Router router = new Router();
-        router.addRoute("/", assignHandlerToMethods(STATUS_OK_HANDLER,
-            new String[]{Method.GET}));
-        router.addRoute("/form", assignHandlerToMethods(STATUS_OK_HANDLER,
-            new String[]{Method.GET, Method.POST, Method.PUT}));
-        router.addRoute("/redirect", assignHandlerToMethods(REDIRECT_HANDLER,
-            Method.all_methods()));
+        router.addRoute("/", unmodifiableResourceRouteHandler(new Resource("")));
+        router.addRoute("/form", resourcefulRouteHandler(new Resource("")));
+        router.addRoute("/redirect", REDIRECT_HANDLER);
+        router.addRoute("/method_options", optionsHandler(Method.allMethods()));
         return router;
     }
 
-    private static RouteHandler assignHandlerToMethods(Function<Request, Response> handler, String[] methods) {
+    private static Function<Request, Response> optionsHandler(String[] allowedMethods) {
+        String[] headers = new String[] {"Allow: " + String.join(",", allowedMethods) + "\r\n"};
+        return new IndifferentHandler(new Response(Status.OK, headers));
+    }
+
+    private static Function<Request, Response> resourcefulRouteHandler(Resource resource) {
         HashMap<String, Function<Request, Response>> methodHandlers = new HashMap<>();
-        for (String method : methods) {
-            methodHandlers.put(method, handler);
+        methodHandlers.put(Method.GET, new GetResourceHandler(resource));
+        for (String method : new String[]{Method.POST, Method.PUT, Method.DELETE}) {
+            methodHandlers.put(method, new ChangeResourceHandler(resource));
         }
+
+        return new RouteHandler(methodHandlers);
+    }
+
+    private static Function<Request, Response> unmodifiableResourceRouteHandler(Resource resource) {
+        HashMap<String, Function<Request, Response>> methodHandlers = new HashMap<>();
+        methodHandlers.put(Method.GET, new GetResourceHandler(resource));
+
         return new RouteHandler(methodHandlers);
     }
 }
--- src/main/java/scarvill/httpserver/constants/Method.java
+++ src/main/java/scarvill/httpserver/constants/Method.java
@@ -8,7 +8,7 @@ public class Method {
     public static final String POST = "POST";
     public static final String DELETE = "DELETE";
 
-    public static String[] all_methods() {
+    public static String[] allMethods() {
         return new String[]{GET, HEAD, OPTIONS, PUT, POST, DELETE};
     }
 }

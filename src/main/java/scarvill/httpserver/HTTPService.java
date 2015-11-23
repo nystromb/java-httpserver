5149f122ef50bd72713f11c3f90b3a978ca51461
Spencer <scarvill91@gmail.com>
2015-11-23 09:51:53 -0600

    Refactor = extract methods in HTTPService

--- src/main/java/scarvill/httpserver/HTTPService.java
+++ src/main/java/scarvill/httpserver/HTTPService.java
@@ -15,16 +15,18 @@ public class HTTPService implements BiConsumer<InputStream, OutputStream> {
         PrintWriter out = new PrintWriter(outputStream, true);
 
         try {
-            String rawRequest = readRequest(in);
-            System.out.print("Received request:\r\n" + rawRequest + "\r\n");
-            Request request = new Request(rawRequest);
-
-            Response response = router.routeRequest(request);
-            System.out.print("Sent response:\r\n" + response.generate() + "\r\n");
-            out.print(response.generate());
-
+            Request request = parseRequest(in);
+            sendResponse(out, request);
             out.close();
-        } catch (IOException e) { throw new RuntimeException(e); }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Request parseRequest(BufferedReader in) throws IOException {
+        String rawRequest = readRequest(in);
+        System.out.print("Received request:\r\n" + rawRequest + "\r\n");
+        return new Request(rawRequest);
     }
 
     private String readRequest(BufferedReader in) throws IOException {
@@ -32,5 +34,11 @@ public class HTTPService implements BiConsumer<InputStream, OutputStream> {
         do rawRequest = rawRequest.concat(Character.toString((char)in.read())); while (in.ready());
 
         return rawRequest;
+    }
+
+    private void sendResponse(PrintWriter out, Request request) {
+        Response response = router.routeRequest(request);
+        System.out.print("Sent response:\r\n" + response.generate() + "\r\n");
+        out.print(response.generate());
     }
 }

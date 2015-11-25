a8cb8f230b734d2a1fad652dcf9cdc110d3dfb60
Spencer <scarvill91@gmail.com>
2015-11-25 14:19:01 -0600

    Implement handler to echo string query parameters

---
+++ src/main/java/scarvill/httpserver/handlers/EchoParametersHandler.java
@@ -0,0 +1,28 @@
+package scarvill.httpserver.handlers;
+
+import scarvill.httpserver.Request;
+import scarvill.httpserver.Response;
+import scarvill.httpserver.constants.Status;
+
+import java.util.HashMap;
+import java.util.function.Function;
+
+public class EchoParametersHandler implements Function<Request, Response> {
+    @Override
+    public Response apply(Request request) {
+        return new Response.Builder()
+            .setStatus(Status.OK)
+            .setBody(assembleResponseBody(request.getParameters()))
+            .build();
+    }
+
+    private String assembleResponseBody(HashMap<String, String> parameters) {
+        String body = "";
+
+        for (String key : parameters.keySet()) {
+            body += key + " = " + parameters.get(key) + "\r\n";
+        }
+
+        return body;
+    }
+}

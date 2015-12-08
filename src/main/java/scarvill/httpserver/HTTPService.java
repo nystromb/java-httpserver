a8e95ee782b0b17ec75f90a71afacb722d243b69
Spencer <scarvill91@gmail.com>
2015-12-08 13:38:35 -0600

    Write response to client socket as raw bytes instead of as String

--- src/main/java/scarvill/httpserver/HTTPService.java
+++ src/main/java/scarvill/httpserver/HTTPService.java
@@ -35,40 +35,49 @@ public class HTTPService implements Serveable {
 
     private void serveRequest() throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedOutputStream out = new BufferedOutputStream(clientSocket.getOutputStream());
 
         Request request = readRequest(in);
         Response response = router.routeRequest(request);
         sendResponse(out, response);
+
         out.close();
     }
 
     private Request readRequest(BufferedReader in) throws IOException {
         String requestLineAndHeaders = readRequestLineAndHeaders(in);
-        logger.logRequest(requestLineAndHeaders);
+        byte[] body = readBody(in);
 
-        return new HTTPRequest(requestLineAndHeaders, readBody(in)).parse();
+        logger.logRequest(requestLineAndHeaders + new String(body));
+
+        return new HTTPRequest(requestLineAndHeaders, body).parse();
     }
 
     private String readRequestLineAndHeaders(BufferedReader in) throws IOException {
         String requestLineAndHeaders = "";
+
         while (!requestLineAndHeaders.contains("\r\n\r\n")) {
             requestLineAndHeaders += in.readLine() + "\r\n";
         }
+
         return requestLineAndHeaders;
     }
 
     private byte[] readBody(BufferedReader in) throws IOException {
         ByteArrayOutputStream body = new ByteArrayOutputStream();
+
         while (in.ready()) {
             body.write(in.read());
         }
+
         return body.toByteArray();
     }
 
-    private void sendResponse(PrintWriter out, Response response) {
+    private void sendResponse(OutputStream out, Response response) throws IOException {
         byte[] rawResponse = new HTTPResponse().generate(response);
+
         logger.logResponse(new String(rawResponse));
-        out.print(new String(rawResponse));
+
+        out.write(rawResponse, 0, rawResponse.length);
     }
 }

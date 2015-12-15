0918c3fe55fb51f602c2a45d199eaf138a91f4a4
Spencer <scarvill91@gmail.com>
2015-12-15 16:13:27 -0600

    Replace FileSystemRouter case statement with hashmap of strategies

--- src/main/java/scarvill/httpserver/routing/FileSystemRouter.java
+++ src/main/java/scarvill/httpserver/routing/FileSystemRouter.java
@@ -9,6 +9,7 @@ import scarvill.httpserver.routing.resource.FileResource;
 import scarvill.httpserver.routing.resource.Resource;
 import scarvill.httpserver.routing.route_strategies.GetRouteOptions;
 import scarvill.httpserver.routing.route_strategies.GetRouteResource;
+import scarvill.httpserver.routing.route_strategies.GiveStaticResponse;
 import scarvill.httpserver.routing.route_strategies.ModifyRouteResource;
 
 import java.nio.file.Files;
@@ -16,10 +17,14 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.function.Function;
 
 public class FileSystemRouter implements Router {
-    private Path publicDirectory;
+    private final Function<Request, Response> NOT_FOUND_STRATEGY =
+        new GiveStaticResponse(new ResponseBuilder().setStatus(Status.NOT_FOUND).build());
+
+    private final Path publicDirectory;
     private VirtualRouter virtualRouter;
 
     public FileSystemRouter(Path publicDirectory) {
@@ -39,21 +44,25 @@ public class FileSystemRouter implements Router {
         if (Files.notExists(filePath)) {
             return virtualRouter.routeRequest(request);
         } else {
-            Resource resource = new FileResource(filePath);
-            switch (request.getMethod()) {
-                case NULL_METHOD:
-                    return new ResponseBuilder().setStatus(Status.METHOD_NOT_ALLOWED).build();
-                case OPTIONS:
-                    Collection<Method> allowedMethods =
-                        Arrays.asList(Method.GET, Method.PUT, Method.POST, Method.DELETE);
-                    return new GetRouteOptions(allowedMethods).apply(request);
-                case GET:
-                    return new GetRouteResource(resource).apply(request);
-                case PATCH:
-                    return new ModifyRouteResource(resource, Status.NO_CONTENT).apply(request);
-                default:
-                    return new ModifyRouteResource(resource).apply(request);
-            }
+            HashMap<Method, Function<Request, Response>> resourceStrategies =
+                generateResourceStrategies(new FileResource(filePath));
+            Function<Request, Response> strategy =
+                resourceStrategies.getOrDefault(request.getMethod(), NOT_FOUND_STRATEGY);
+            return strategy.apply(request);
         }
+    }
+
+    private HashMap<Method, Function<Request, Response>> generateResourceStrategies(Resource resource) {
+        HashMap<Method, Function<Request, Response>> resourceStrategies = new HashMap<>();
+        resourceStrategies.put(Method.GET, new GetRouteResource(resource));
+        resourceStrategies.put(Method.PUT, new ModifyRouteResource(resource));
+        resourceStrategies.put(Method.POST, new ModifyRouteResource(resource));
+        resourceStrategies.put(Method.PATCH, new ModifyRouteResource(resource, Status.NO_CONTENT));
+        resourceStrategies.put(Method.DELETE, new ModifyRouteResource(resource));
+        Collection<Method> allowedMethods = Arrays.asList(
+            Method.GET, Method.PUT, Method.POST, Method.PATCH, Method.DELETE, Method.OPTIONS);
+        resourceStrategies.put(Method.OPTIONS, new GetRouteOptions(allowedMethods));
+
+        return resourceStrategies;
     }
 }

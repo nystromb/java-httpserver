b20387666431ea36138798451bd5496624db5ed9
Spencer <scarvill91@gmail.com>
2015-12-15 16:00:46 -0600

    Implement routing OPTIONS and PATCH requests in FileSystemRouter

--- src/main/java/scarvill/httpserver/routing/FileSystemRouter.java
+++ src/main/java/scarvill/httpserver/routing/FileSystemRouter.java
@@ -7,13 +7,15 @@ import scarvill.httpserver.response.ResponseBuilder;
 import scarvill.httpserver.response.Status;
 import scarvill.httpserver.routing.resource.FileResource;
 import scarvill.httpserver.routing.resource.Resource;
+import scarvill.httpserver.routing.route_strategies.GetRouteOptions;
 import scarvill.httpserver.routing.route_strategies.GetRouteResource;
-import scarvill.httpserver.routing.route_strategies.GiveStaticResponse;
 import scarvill.httpserver.routing.route_strategies.ModifyRouteResource;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.function.Function;
 
 public class FileSystemRouter implements Router {
@@ -41,8 +43,14 @@ public class FileSystemRouter implements Router {
             switch (request.getMethod()) {
                 case NULL_METHOD:
                     return new ResponseBuilder().setStatus(Status.METHOD_NOT_ALLOWED).build();
+                case OPTIONS:
+                    Collection<Method> allowedMethods =
+                        Arrays.asList(Method.GET, Method.PUT, Method.POST, Method.DELETE);
+                    return new GetRouteOptions(allowedMethods).apply(request);
                 case GET:
                     return new GetRouteResource(resource).apply(request);
+                case PATCH:
+                    return new ModifyRouteResource(resource, Status.NO_CONTENT).apply(request);
                 default:
                     return new ModifyRouteResource(resource).apply(request);
             }
--- src/test/java/scarvill/httpserver/routing/FileSystemRouterTest.java
+++ src/test/java/scarvill/httpserver/routing/FileSystemRouterTest.java
@@ -8,15 +8,11 @@ import scarvill.httpserver.response.Response;
 import scarvill.httpserver.response.ResponseBuilder;
 import scarvill.httpserver.response.Status;
 import scarvill.httpserver.routing.route_strategies.GiveStaticResponse;
-import scarvill.httpserver.server.Logger;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,36 +20,22 @@ import static org.junit.Assert.*;
 
 public class FileSystemRouterTest {
     @Test
-    public void testDelegatesManuallyConfiguredRoutesToVirtualRouter() {
-        Request request = new RequestBuilder().setMethod(Method.GET).setURI("/").build();
-        Status expectedResponseStatus = Status.OK;
-        Router router = new FileSystemRouter(Paths.get("/foo"));
+    public void testDelegatesRoutingToVirtualRouterWhenRequestRouteIsNotInRouterDirectory() {
+        Router router = new FileSystemRouter(Paths.get("/noFooBarFileHere"));
+        Request request = new RequestBuilder().setMethod(Method.GET).setURI("/foo.bar").build();
         Response response = new ResponseBuilder().setStatus(Status.OK).build();
-        router.addRoute("/", Method.GET, new GiveStaticResponse(response));
+        router.addRoute("/foo.bar", Method.GET, new GiveStaticResponse(response));
 
         Response routerResponse = router.routeRequest(request);
 
-        assertEquals(expectedResponseStatus, routerResponse.getStatus());
+        assertEquals(Status.OK, routerResponse.getStatus());
     }
 
     @Test
-    public void testReturnsNotFoundResponseWhenRequestRouteIsNotInRouterDirectory() throws IOException {
+    public void testReturnsFilesInRouterDirectory() throws IOException {
         Path publicDirectory = Files.createTempDirectory("public");
-        Router router = new FileSystemRouter(publicDirectory);
-        Request request = new RequestBuilder().setURI("/unconfigured").build();
-
-        Response response = router.routeRequest(request);
-
-        assertEquals(Status.NOT_FOUND, response.getStatus());
-
-        Files.delete(publicDirectory);
-    }
-
-    @Test
-    public void testReturnsFileWhenGetRequestRouteIsInRouterDirectory() throws IOException {
-        Path publicDirectory = Files.createTempDirectory("public");
-        List<String> fileContents = Arrays.asList("Line1", "Line2");
-        Path file = createTempFileWithContent(publicDirectory, "file", "tmp", fileContents);
+        String fileContents = "file contents";
+        Path file = createTempFileWithContent(publicDirectory, "file", "tmp", fileContents.getBytes());
 
         Router router = new FileSystemRouter(publicDirectory);
         Request request = new RequestBuilder()
@@ -63,19 +45,19 @@ public class FileSystemRouterTest {
 
         Response response = router.routeRequest(request);
 
-        assertEquals("Line1\nLine2\n", new String(response.getBody()));
+        assertEquals(fileContents, new String(response.getBody()));
 
         deleteFiles(Arrays.asList(file, publicDirectory));
     }
 
     @Test
-    public void testModifiesFileWhenGetRequestRouteIsInRouterDirectory() throws IOException {
+    public void testModifiesFilesInRouterDirectory() throws IOException {
         Path publicDirectory = Files.createTempDirectory("public");
-        List<String> initialFileContents = Arrays.asList("Line1", "Line2");
+        byte[] initialFileContents = "foo".getBytes();
         Path file = createTempFileWithContent(publicDirectory, "file", "tmp", initialFileContents);
 
         Router router = new FileSystemRouter(publicDirectory);
-        String newFileContents = "new file contents\n";
+        String newFileContents = "new file contents";
         Request request = new RequestBuilder()
             .setMethod(Method.PUT)
             .setURI("/" + file.getFileName())
@@ -89,8 +71,53 @@ public class FileSystemRouterTest {
         deleteFiles(Arrays.asList(file, publicDirectory));
     }
 
+    @Test
+    public void testPatchesFilesInRouterDirectory() throws IOException {
+        Path publicDirectory = Files.createTempDirectory("public");
+        byte[] initialFileContents = "foo".getBytes();
+        Path file = createTempFileWithContent(publicDirectory, "file", "tmp", initialFileContents);
+
+        Router router = new FileSystemRouter(publicDirectory);
+        String newFileContents = "new file contents";
+        Request request = new RequestBuilder()
+            .setMethod(Method.PATCH)
+            .setURI("/" + file.getFileName())
+            .setBody(newFileContents.getBytes())
+            .build();
+
+        Response response = router.routeRequest(request);
+
+        assertEquals(Status.NO_CONTENT, response.getStatus());
+        assertEquals(newFileContents, new String(Files.readAllBytes(file)));
+
+        deleteFiles(Arrays.asList(file, publicDirectory));
+    }
+
+    @Test
+    public void testRoutesOptionsRequestsForExtantResources() throws IOException {
+        Path publicDirectory = Files.createTempDirectory("public");
+        Path file = createTempFileWithContent(publicDirectory, "file", "tmp", "".getBytes());
+
+        Router router = new FileSystemRouter(publicDirectory);
+        Request request = new RequestBuilder()
+            .setMethod(Method.OPTIONS)
+            .setURI("/" + file.getFileName())
+            .build();
+
+        Response response = router.routeRequest(request);
+
+        assertEquals(Status.OK, response.getStatus());
+        assertTrue(response.getHeaders().get("Allow").contains(Method.GET.toString()));
+        assertTrue(response.getHeaders().get("Allow").contains(Method.PUT.toString()));
+        assertTrue(response.getHeaders().get("Allow").contains(Method.POST.toString()));
+        assertTrue(response.getHeaders().get("Allow").contains(Method.DELETE.toString()));
+        assertTrue(response.getHeaders().get("Allow").contains(Method.OPTIONS.toString()));
+
+        deleteFiles(Arrays.asList(file, publicDirectory));
+    }
+
     private Path createTempFileWithContent(
-        Path dir, String prefix, String suffix, Iterable<String> content) throws IOException {
+        Path dir, String prefix, String suffix, byte[] content) throws IOException {
         Path file = Files.createTempFile(dir, prefix, suffix);
         Files.write(file, content);
 

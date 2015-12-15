066ffcf6a4958d38d798d65de0bfee4b53803597
Spencer <scarvill91@gmail.com>
2015-12-15 15:15:52 -0600

    Implement GetRouteOptions routing strategy

---
+++ src/main/java/scarvill/httpserver/routing/route_strategies/GetRouteOptions.java
@@ -0,0 +1,38 @@
+package scarvill.httpserver.routing.route_strategies;
+
+import scarvill.httpserver.request.Method;
+import scarvill.httpserver.request.Request;
+import scarvill.httpserver.response.Response;
+import scarvill.httpserver.response.ResponseBuilder;
+import scarvill.httpserver.response.Status;
+
+import java.util.Collection;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class GetRouteOptions implements Function<Request, Response> {
+    private Collection<Method> allowedMethods;
+
+    public GetRouteOptions(Collection<Method> allowedMethods) {
+        this.allowedMethods = allowedMethods;
+    }
+
+    @Override
+    public Response apply(Request request) {
+        return new ResponseBuilder()
+                .setStatus(Status.OK)
+                .setHeader("Allow", allowedMethodsString(allowedMethods))
+                .build();
+    }
+
+    private String allowedMethodsString(Collection<Method> allowedMethods) {
+        String methodsString =
+            allowedMethods.stream().map(Method::toString).collect(Collectors.joining(","));
+
+        if (!methodsString.contains(Method.OPTIONS.toString())) {
+            methodsString += "," + Method.OPTIONS.toString();
+        }
+
+        return methodsString;
+    }
+}
---
+++ src/test/java/scarvill/httpserver/routing/route_strategies/GetRouteOptionsTest.java
@@ -0,0 +1,31 @@
+package scarvill.httpserver.routing.route_strategies;
+
+import org.junit.Test;
+import scarvill.httpserver.request.Method;
+import scarvill.httpserver.request.Request;
+import scarvill.httpserver.request.RequestBuilder;
+import scarvill.httpserver.response.Response;
+import scarvill.httpserver.response.Status;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.function.Function;
+
+import static org.junit.Assert.*;
+
+public class GetRouteOptionsTest {
+
+    @Test
+    public void testReturnsOptionsResponse() {
+        Request request = new RequestBuilder().setMethod(Method.OPTIONS).build();
+        Collection<Method> allowedMethods = Arrays.asList(Method.GET, Method.HEAD, Method.OPTIONS);
+        Function<Request, Response> optionsStrategy = new GetRouteOptions(allowedMethods);
+
+        Response response = optionsStrategy.apply(request);
+
+        assertEquals(Status.OK, response.getStatus());
+        assertTrue(response.getHeaders().get("Allow").contains(Method.GET.toString()));
+        assertTrue(response.getHeaders().get("Allow").contains(Method.HEAD.toString()));
+        assertTrue(response.getHeaders().get("Allow").contains(Method.OPTIONS.toString()));
+    }
+}
